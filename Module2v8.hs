a1519bcf9c3a2bc834966fe75cc6df58dc263e0b
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-11-20 18:57:14 +0300

    Solve some more tasks from 2v8

--- Module2v8.hs
+++ Module2v8.hs
@@ -23,9 +23,30 @@ chinese ((a₁,m₁):xs) = chineseGo xs (a₁ `mod` m₁) m₁
             c' = c + mprod * y
         in chineseGo xs c' (mprod * m)
 
+------ 2.18
+
 e218 = do
     print $ chinese [(3,7), (4,9)]
     print $ chinese [(137,423), (87,191)]
     print $ chinese [(133,451), (87,237)]
     print $ chinese [(5,9), (6,10), (7,11)]
     print $ chinese [(37,43), (22,49), (18,71)]
+
+{-
+λ> e218
+52
+27209
+49743
+194
+139959
+-}
+
+
+------ 2.19
+
+-- 23
+e219 = chinese [(2,3), (3,5), (2,7)]
+
+
+------ 2.20 on the paper
+------ 2.21 on the paper
